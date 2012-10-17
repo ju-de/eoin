c400fd90c764eefa7c597e77fd8f91ed6a67595f
James Yu <jj6yu@uwaterloo.ca>
2012-10-17 04:24:58 +0000

    Tiny bit more cleaning of the code. Removed unnecessary comments and grouped code a little bit better. I really need to get in the habit of committing less.

--- src/dmcigd/Demo.java
+++ src/dmcigd/Demo.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Demo {
 	
 	private int x;
+	private int speed;
 	
 	//Initialize visible objects list
 	private ArrayList visibleObjects = new ArrayList();
@@ -17,13 +18,21 @@ public class Demo {
 	public Demo() {
 		
 		x = 1;
+		speed = 1;
 		visibleObjects.add((int) 1);
 		
 	}
 	
 	public void step() {
+		
+		if(x >= 630) {
+			speed = -1;
+		}else if(x <= 0) {
+			speed = 1;
+		}
 			
-		x++;
+		x = x + speed;
+		
 		visibleObjects.set(0, (int) x);
 		
 	}
