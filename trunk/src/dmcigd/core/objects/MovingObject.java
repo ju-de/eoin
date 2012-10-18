46517b15a4c104e1334881bf2be5cb62d7a7e7a2
yu.james1996@gmail.com <yu.james1996@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-10-18 19:05:10 +0000

    Modified MovingObject class to account for all sizes of bounding boxes. This fix introduced a glitch with accelerating through walls, this will be resolved when I get home.

--- trunk/src/dmcigd/core/objects/MovingObject.java
+++ trunk/src/dmcigd/core/objects/MovingObject.java
@@ -11,8 +11,10 @@ public class MovingObject {
 	private int vy = 2;
 	private int ax = 0;
 	private int ay = 0;
+	private int height = 32;
+	private int width = 32;
 	
-	//Public position getters (if level needs to check for position or speed)
+	//Public getters
 	public int getX() {
 		return x;
 	}
@@ -29,7 +31,15 @@ public class MovingObject {
 		return vy;
 	}
 	
-	//Public position setters (if level needs to move player around dynamically)
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	//Public setters
 	public void setX(int x) {
 		this.x = x;
 	}
@@ -54,6 +64,14 @@ public class MovingObject {
 		this.ay = ay;
 	}
 	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
 	private boolean isSolid(char block) {
 		if(Arrays.asList(' ', '1').contains(block)) {
 			return false;
@@ -68,64 +86,61 @@ public class MovingObject {
 		vx = vx + ax;
 		vy = vy + ay;
 		
-		//Cap all speeds at 32px per tick
-		if(vx > 32) {
-			vx = 32;
+		//Cap all speeds at 30px per tick
+		if(vx + ax > 30) {
+			vx = 30;
 			ax = 0;
-		}else if(vx < -32) {
-			vx = -32;
+		}else if(vx + ax < -30) {
+			vx = -30;
 			ax = 0;
 		}
-		if(vy > 32) {
-			vy = 32;
+		if(vy + ay > 30) {
+			vy = 30;
 			ay = 0;
-		}else if(vy < -32) {
-			vy = -32;
+		}else if(vy + ay < -30) {
+			vy = -30;
 			ay = 0;
 		}
 		
 		if(vx > 0) {
 			//Move Right
 			//If not crossing a tile, move over
-			if(((x-1)/32) == (x-1 + vx)/32) {
+			if(((x - width + 31)/32) == (x + vx - width + 31)/32) {
 				x = x + vx;
 			} else {
+				//Check for case of object sitting between tiles
 				int column = 3;
-				if(x % 32 == 0) {
+				if(x % 32 <= 32 - width) {
 					column = 2;
 				}
 				//Check if tile to the immediate right is clear
 				if(!isSolid(immediateBlocks[1][column])) {
 					//If sitting perfectly in tile, move
-					if(y % 32 == 0) {
+					if((y - height + 32) % 32 == 0) {
 						x = x + vx;
 					} else {
 						//If sitting between tiles, if tile to bottom right is also clear, move
 						if(!isSolid(immediateBlocks[2][column])) {
 							x = x + vx;
 						} else {
-							x = (x + vx) / 32 * 32;
-							vx = 0;
-							ax = 0;
+							x = ((x + vx) / 32 * 32) - width + 32;
 						}
 					}
 				} else {
-					x = (x + vx) / 32 * 32;
-					vx = 0;
-					ax = 0;
+					x = ((x + vx) / 32 * 32) - width + 32;
 				}
 			}
 			
 		}else if(vx < 0) {
 			//Move left
 			//If not crossing a tile, move over
-			if(((x)/32) == (x-1 + vx)/32) {
+			if(((x)/32) <= (x-1 + vx)/32) {
 				x = x + vx;
 			} else {
 				//Check if tile to the immediate left is clear
 				if(!isSolid(immediateBlocks[1][0])) {
 					//If sitting perfectly in tile, move
-					if(y % 32 == 0) {
+					if(y % 32 <= 32 - height) {
 						x = x + vx;
 					} else {
 						//If sitting between tiles, if tile to bottom left is also clear, move
@@ -143,39 +158,40 @@ public class MovingObject {
 		
 		if(vy > 0) {
 			//Move Down
-			if(((y-1)/32) == (y-1 + vy)/32) {
+			if(((y + height - 33)/32) == (y + vy + height - 33)/32) {
 				y = y + vy;
 			} else {
+				//Check for case of object sitting between tiles
 				int row = 3;
-				if(y % 32 == 0) {
+				if(y % 32 <= 32 - height) {
 					row = 2;
 				}
 				//Check if tile to the immediate bottom is clear
 				if(!isSolid(immediateBlocks[row][1])) {
 					//If sitting perfectly in tile, move
-					if(x % 32 == 0) {
+					if((x  - width + 32) % 32 == 0) {
 						y = y + vy;
 					} else {
 						//If sitting between tiles, if tile to bottom right is also clear, move
 						if(!isSolid(immediateBlocks[row][2])) {
 							y = y + vy;
 						} else {
-							y = (y + vy) / 32 * 32;
+							y = ((y + vy) / 32 * 32) - height + 32;
 						}
 					}
 				} else {
-					y = (y + vy) / 32 * 32;
+					y = ((y + vy) / 32 * 32) - height + 32;
 				}
 			}
 		}else if(vy < 0) {
 			//Move Up
-			if(((y)/32) == (y + vy)/32) {
+			if(((y)/32) <= (y + vy)/32) {
 				y = y + vy;
 			} else {
 				//Check if tile to the immediate top is clear
 				if(!isSolid(immediateBlocks[0][1])) {
 					//If sitting perfectly in tile, move
-					if(x % 32 == 0) {
+					if(x % 32 <= 32 - width) {
 						y = y + vy;
 					} else {
 						//If sitting between tiles, if tile to top right is also clear, move
--- trunk/src/dmcigd/core/objects/player/Player.java
+++ trunk/src/dmcigd/core/objects/player/Player.java
@@ -1,13 +1,14 @@
 package dmcigd.core.objects.player;
 
 import dmcigd.core.objects.*;
-import java.util.Arrays;
 
 public class Player extends MovingObject {
 	
 	public Player(int x, int  y) {
 		setX(x);
 		setY(y);
+		setHeight(16);
+		setWidth(16);
 	}
 	
 	public void step(char[][] immediateBlocks) {
