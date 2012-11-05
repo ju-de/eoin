275a49945c646aa2f7415ac966b0b174f3d2f964
hussainmkj <hussainmkj@gmail.com>
2012-11-05 03:26:06 +0000

    Revamped DialogueHandler again, revamped DialogueItem

--- src/dmcigd/core/DialogueHandler.java
+++ src/dmcigd/core/DialogueHandler.java
@@ -1,55 +1,77 @@
 package dmcigd.core;
 
-import java.util.ArrayList;
 import dmcigd.core.DialogueItem;
 import java.util.regex.*;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ListIterator;
 
 public class DialogueHandler {
 	// the internal ArrayList of DialogueItem elements
-	private ArrayList<DialogueItem> dialogueItems;
+	private List<DialogueItem> dialogueItems;
+	private ListIterator<DialogueItem> dialogueItemIterator;
+	private DialogueItem currentDialogueItem;
+		private ArrayList<String> currentScreen;
+	private List<String> currentView;
+	private boolean inDialogue;
+	private int linePosition;
+	public static final int maxWidth = 42;
+	public static final int maxLines = 3;
 
-	public void progressDialogue(ArrayList<DialogueItem> dialogueItems) {
-		this.dialogueItems = new ArrayList<DialogueItem>(dialogueItems);
+	public boolean inDialogue() {
+		return inDialogue();
 	}
-	// overloaded constructors to deal with more intuitive parameters
-	public void DialogueHandler(ArrayList<String[]> dialogueItems) {
-		this.dialogueItems = dialogueItemsFromList(dialogueItems);
+	public int lines() {
+		return currentView.size();
 	}
-	public void DialogueHandler(String[][] dialogueItems) {
-		this.dialogueItems = dialogueItemsFromList(new ArrayList<String[]>(Arrays.asList(dialogueItems)));
+	public String line(int l) {
+		if(l < 1 || l > currentView.size())
+			throw new IllegalArgumentException("line number out of bounds");
+		return currentView.get(l-1);
 	}
 
-// generates an ArrayList of DialogueItem elements given
-// an ArrayList of string arrays of the required fields
-//
-// string array elements are in the order 
-// avatarImageCode, name, text
-	public ArrayList<DialogueItem> dialogueItemsFromList(ArrayList<String[]> dialogueList) {
-		ArrayList<DialogueItem> dialogueItems = new ArrayList<DialogueItem>();
-		// holds the list of prepared dialogue text arrays for each iteration
-ArrayList<String[]> itemText = new ArrayList<String[]>();
-		for(String[] element : dialogueList) {
-			// append the processed dialogue text
-			itemText.addAll(prepareDialogueText(element[2]));
-			// iterate through the list of dialogue text arrays,
-			// generating a new DialogueItem with the same avatarImageCode and name for each
-			for(String[] text : itemText) 
-				dialogueItems.add(new DialogueItem(element[0], element[1], text));
-			// clear the dialogue text list for the next iteration
-			itemText.clear();
-		}
-		return dialogueItems;
+	public DialogueHandler() {
+		dialogueItems = Collections.synchronizedList(new ArrayList<DialogueItem>());
+		dialogueItemIterator = dialogueItems.listIterator();
+		inDialogue = false;
+	}
+
+// clears current dialogue and adds new dialogue
+	public boolean setDialogueItems(ArrayList<DialogueItem> dialogueItems) {
+		this.dialogueItems.clear();
+		this.dialogueItems.addAll(dialogueItems);
+		return startDialogue();
+	}
+	// overloaded methods to deal with more intuitive parameters
+	public boolean setDialogue(ArrayList<String[]> dialogueItems) {
+		this.dialogueItems.clear();
+		for(String[] dialogueItem : dialogueItems)
+			add(dialogueItem);
+		return startDialogue();
+	}
+	public boolean setDialogue(String[][] dialogueItems) {
+		this.dialogueItems.clear();
+		for(String[] dialogueItem : dialogueItems)
+			add(dialogueItem);
+		return startDialogue();
+	}
+
+	public boolean startDialogue() {
+		if(dialogueItems.isEmpty())
+			return false;
+		inDialogue = true;
+		updateScreen();
+		return true;
+	}
+	public void closeDialogue() {
+		inDialogue = false;
 	}
 
 // add a new dialogue element
 	public void add(String avatarImageCode, String name, String text) {
-		// the list of processed dialogue text arrays
-		ArrayList<String[]> preparedText = prepareDialogueText(text);
-		// iterate through the dialogue text arrays,
-		// generating a new DialogueItem with the same avatarImageCode and name for each
-		for(String[] element : preparedText)
-			dialogueItems.add(new DialogueItem(avatarImageCode, name, element));
+		dialogueItems.add(new DialogueItem(avatarImageCode, name, text));
 	}
 	// overloaded definitions for convenience
 	public void add(ArrayList<String> dialogue) {
@@ -61,32 +83,40 @@ ArrayList<String[]> itemText = new ArrayList<String[]>();
 		add(new ArrayList<String>(Arrays.asList(dialogue)));
 	}
 
-// takes a text string and separates it into 
-// a list of text arrays (dialogue parts) with no more than DialogueItem.maxLines string elements,
-// each string (line)  with no more than DialogueItem.maxWidth characters
-	public ArrayList<String[]> prepareDialogueText(String text) {
+// takes a text string and separates it into
+// a list of strings no more than maxWidth in length
+	public ArrayList<String> prepareDialogueText(String text) {
 		// the list of dialogue parts
-		ArrayList<String[]> preparedText = new ArrayList<String[]>();
-		// the initial array of dialogue parts
-		String[] splitText;
-		// if the parameter size is less than the max width, no need to separate it
-		if(text.length() <= DialogueItem.maxWidth) {
-			preparedText.add(new String[] {text});
-			return preparedText;
-		}
-		// regex for splitting a dialogue part into lines of at most DialogueItem.maxWidth length
-		String lineRegex = ".{1," + DialogueItem.maxWidth + "}\\s";
-		// regex for splitting the text into dialogue parts of no more than DialogueItem.maxLines * DialogueItem.maxWidth length
-		String partRegex = ".{" + DialogueItem.maxWidth + "," + DialogueItem.maxLines * DialogueItem.maxWidth + "}\\s";
-		// split the text into an array of dialogue parts
-		splitText = Pattern.compile(partRegex).split(text);
-		// iterate through the dialogue parts, split each into an array of lines and append to the final variable
-		for(String l : splitText)
-			preparedText.add(Pattern.compile(lineRegex).split(l));
+		ArrayList<String> preparedText = new ArrayList<String>();
+		// regex for splitting a line into chunks of at most maxWidth length
+		String lineRegex = ".{0," + maxWidth + "}\\s";
+		preparedText.addAll(Arrays.asList(Pattern.compile(lineRegex).split(text)));
 		return preparedText;
 	}
 
-	public ArrayList<DialogueItem> dialogueItems() {
-		return dialogueItems;
+	public void updateScreen() {
+		if(!dialogueItemIterator.hasNext()) {
+			closeDialogue();
+			return;
+		}
+		currentScreen = prepareDialogueText(dialogueItemIterator.next().text());
+		updateView(0);
+	}
+	public void updateView(int position) {
+		if(position > currentScreen.size() || position < 0)
+			throw new IllegalArgumentException("position out of bounds");
+		int endPosition = position + maxLines;
+		if(endPosition > currentScreen.size()) {
+			if(position > 0) {
+				updateScreen();
+				return;
+			}
+			endPosition = currentScreen.size();
+		}
+		linePosition = position;
+		currentView = currentScreen.subList(position, endPosition);
+	}
+	public void advance() {
+		updateView(linePosition + 1);
 	}
 }
