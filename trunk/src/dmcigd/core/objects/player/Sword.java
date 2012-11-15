632f2a0a831584dfaa6587832b84ffb5204ee45b
yu.james1996@gmail.com <yu.james1996@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-11-15 05:45:38 +0000

    Murder

--- trunk/src/dmcigd/core/objects/player/Sword.java
+++ trunk/src/dmcigd/core/objects/player/Sword.java
@@ -21,7 +21,7 @@ public class Sword extends ObjectCollision {
 	//Public setters
 	public void setPosition(int x, int y) {
 		baseX = x;
-		baseY = y + 16;
+		baseY = y + 10;
 	}
 	public void setLadder(boolean onLadder) {
 		this.onLadder = onLadder;
@@ -29,7 +29,7 @@ public class Sword extends ObjectCollision {
 	public void attack() {
 		if(!onLadder && !attacking) {
 			this.attacking = true;
-			setSequence(5);
+			setSequence(1);
 			setFrame(0);
 		}
 	}
@@ -37,25 +37,25 @@ public class Sword extends ObjectCollision {
 	public Sword(int x, int y, ArrayList<SolidObject> solidObjects) {
 		
 		baseX = x;
-		baseY = y + 16;
+		baseY = y;
 		
 		setX(baseX);
 		setY(baseY);
-		setHeight(14);
-		setWidth(20);
-		setImageHeight(32);
-		setImageWidth(32);
+		setHeight(16);
+		setWidth(30);
+		setImageHeight(30);
+		setImageWidth(30);
 		
-		setMapCode("`");
-		setImagePath("objects.gif");
+		setMapCode("sword");
+		setImagePath("sword.gif");
 		
 		this.solidObjects = solidObjects;
 		
-		setSequence(5);
+		setSequence(0);
 		setFrame(0);
 		
-		setFrameLimits(new int[] {2,6,10,10,10,6});
-		setAnimationLoops(new boolean[] {false,false,false,false,false,false});
+		setFrameLimits(new int[] {2,6});
+		setAnimationLoops(new boolean[] {false,false});
 		setFrameSpeed(0.4f);
 		
 	}
@@ -76,17 +76,24 @@ public class Sword extends ObjectCollision {
 	
 	public void step() {
 		if(onLadder) {
-			setSequence(5);
+			
+			setSequence(0);
 			setFrame(1);
-			setX(baseX);
-			setY(baseY);
+			
+			setX(baseX - 4);
+			setY(baseY + 2);
+			
+			attacking = false;
 		}else if(attacking) {
+			
+			//Offset
+			int offSet = getFrame() * getFrame();
 			
 			//Set Position
 			if(flipped) {
-				setX(baseX - (getFrame()*2));
+				setX(baseX - offSet - 10);
 			} else {
-				setX(baseX + (getFrame()*2));
+				setX(baseX + offSet);
 			}
 			setY(baseY);
 			
@@ -99,9 +106,15 @@ public class Sword extends ObjectCollision {
 				attacking = false;
 			}
 		}else {
-			setSequence(5);
+			setSequence(0);
 			setFrame(0);
-			setX(baseX);
+			
+			if(flipped) {
+				setX(baseX-10);
+			} else {
+				setX(baseX);
+			}
+			
 			setY(baseY);
 		}
 	}
