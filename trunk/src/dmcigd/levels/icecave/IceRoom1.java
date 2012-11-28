4d9a195494c879c4308767e8ac498edc7a161e17
jrsylightning@gmail.com <jrsylightning@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-11-28 02:17:02 +0000

    ICE CAVE ROOM 1 AVAILABLE (literally just a pit-fall)
    added: 
    - renamed ice.gif -> icecave.gif
    - added /share/txt/maps/icecave/IceRoom1
    	- blockmap + envmap
    - MainMenu.java START GAME leads to IceRoom1
    - dmcigd/levels/icecave
    	- IceRoom1.java
    	
    (room2+3 not on this one)

---
+++ trunk/src/dmcigd/levels/icecave/IceRoom1.java
@@ -0,0 +1,17 @@
+package dmcigd.levels.icecave;
+
+import java.net.URL;
+
+import dmcigd.core.objects.*;
+import dmcigd.core.objects.npc.Sign;
+import dmcigd.core.objects.regions.*;
+import dmcigd.core.room.*;
+
+public class IceRoom1 extends Room implements Runnable{
+	public IceRoom1(URL codeBase) {
+		super(codeBase, "icecave", "IceRoom1", "icy");
+	}
+	public void initializeRoom(){
+		addRegion(new Sign(66 * 32, 37 * 32, 6, "Placeholder:", "Door Goes Here.", getDialogueHandler()));
+	}
+}
