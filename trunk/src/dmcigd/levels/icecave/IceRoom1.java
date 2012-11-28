b21b4671e743a92d4012ceda2e69a0ed1adea83e
yu.james1996@gmail.com <yu.james1996@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-11-28 04:14:20 +0000

    Really minor map clean-up, fine tuning, and graphical updates.

--- trunk/src/dmcigd/levels/icecave/IceRoom1.java
+++ trunk/src/dmcigd/levels/icecave/IceRoom1.java
@@ -2,8 +2,6 @@ package dmcigd.levels.icecave;
 
 import java.net.URL;
 
-import dmcigd.core.objects.*;
-import dmcigd.core.objects.npc.Sign;
 import dmcigd.core.objects.regions.*;
 import dmcigd.core.room.*;
 
@@ -12,6 +10,6 @@ public class IceRoom1 extends Room implements Runnable{
 		super(codeBase, "icecave", "IceRoom1", "icy");
 	}
 	public void initializeRoom(){
-		addRegion(new Sign(66 * 32, 33 * 32, 6, "Placeholder:", "Door Goes Here.", getDialogueHandler()));
+		addRegion(new RoomPassage(66 * 32, 33 * 32, "game.MainMenu"));
 	}
 }
