db2ab39358b523cd14d3ada170535bd3e4dfc94e
belieber10423 <belieber10423@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-11-30 01:09:07 +0000

    Deer sublevel 1 layout finished

--- trunk/src/dmcigd/levels/deer/One.java
+++ trunk/src/dmcigd/levels/deer/One.java
@@ -3,6 +3,7 @@ package dmcigd.levels.deer;
 import java.net.URL;
 
 import dmcigd.core.room.*;
+import dmcigd.core.objects.npc.Knight;
 import dmcigd.core.objects.npc.Sign;
 
 
@@ -13,7 +14,12 @@ public class One extends Room implements Runnable{
 	}
 	
 	public void initializeRoom() {
-		addRegion(new Sign(24 * 32, 12 * 32, 9, "SIGN", "Deer Village Ahead.\nBEWARE THE WOLVES!", getDialogueHandler()));
+		addRegion(new Sign(24 * 32 + 8, 12 * 32, 6, "SIGN", "Deer Village Ahead.\nBEWARE THE WOLVES!", getDialogueHandler()));
+		addRegion(new Sign(164 * 32, 28 * 32, 6, "SIGN", "Deer Village", getDialogueHandler()));
+		
+		addRegion(new Knight(50 * 32, 24 * 32, false, "Careful! These wolves may look cute but they're far from harmless!", getDialogueHandler()));
+		addRegion(new Knight(113 * 32, 23 * 32, false, "Somedays, I like to run and jump through the air...\nIt's almost like I'm flying...", getDialogueHandler()));
+		addRegion(new Knight(154 * 32, 24 * 32, true, "The deer village is just ahead. Remember your mission and try not to get too involved with their affairs... the deer people can be quite strange at times.", getDialogueHandler()));
 	}
 	
 }
