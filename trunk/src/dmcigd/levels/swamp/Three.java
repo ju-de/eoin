654c2719ec0f213d84b407b28b96ccd2af79bbeb
yu.james1996@gmail.com <yu.james1996@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-11-30 03:17:06 +0000

    Set up swamp level packages and blockmaps

---
+++ trunk/src/dmcigd/levels/swamp/Three.java
@@ -0,0 +1,17 @@
+package dmcigd.levels.swamp;
+
+import dmcigd.core.room.*;
+
+import java.net.URL;
+
+public class Three extends Room {
+
+	public Three(URL codeBase) {
+		super(codeBase, "swamp", "One", "grassy");
+	}
+
+	public void initializeRoom(){
+		
+		
+	}
+}
