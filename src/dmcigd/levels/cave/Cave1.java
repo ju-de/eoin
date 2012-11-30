33bbb8d480b789b0202843a67453a525588abcd7
James Yu <jj6yu@uwaterloo.ca>
2012-11-30 01:45:56 +0000

    Help with starting off the cave level

--- src/dmcigd/levels/cave/Cave1.java
+++ src/dmcigd/levels/cave/Cave1.java
@@ -1,14 +1,16 @@
 package dmcigd.levels.cave;
 
+import dmcigd.core.room.*;
+
 import java.net.URL;
 
-public class Cave1 {
+public class Cave1 extends Room {
 
 	public Cave1(URL codeBase) {
 		super(codeBase, "cave", "Cave1", "rocky");
 	}
 
-	public void initRoom(){
+	public void initializeRoom(){
 		
 		
 	}
