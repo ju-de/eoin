580b7da653afaa1a4340588789bce3517ecfbf4d
necrophiliator@gmail.com <necrophiliator@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-01 08:04:13 +0000

    Playing around on the Ogre level (NPCS/SIGNS) - Alan

--- trunk/src/dmcigd/levels/ogre/One.java
+++ trunk/src/dmcigd/levels/ogre/One.java
@@ -1,5 +1,6 @@
 package dmcigd.levels.ogre;
 
+import dmcigd.core.objects.npc.Knight;
 import dmcigd.core.objects.npc.Sign;
 import dmcigd.core.room.Room;
 
@@ -12,6 +13,10 @@ public class One extends Room implements Runnable{
 	}
 	
 	public void initializeRoom() {
-		addRegion(new Sign(35 * 15, 35 * 15, 3, "SIGN", "Entering Ogre Territory.\nWATCH YOUR STEP!", getDialogueHandler()));
+		addRegion(new Sign(73 * 32, 21 * 32, 6, "SIGN", "Entering Ogre Territory.\nWATCH YOUR STEP!", getDialogueHandler()));
+		
+		addRegion(new Knight(50 * 32 - 4, 15 * 32, false, "Congratulations on getting past the first part of your quest - unfortunately, I have to notify you that our forces are more scattered around these parts.", getDialogueHandler()));
+		addRegion(new Knight(142 * 32 - 4, 23 * 32, false, "Ogres lurk about - they may seem timid and frightened but they're really sly and cunning creatures. /nThe hole before you is an ogre trap - they set these up to catch their prey", getDialogueHandler()));
+		addRegion(new Knight(162 * 32 - 4, 24 * 32, false, "How do Ogres remove their prey from their traps? Well, um. That is a very good question. /nWell, I suppose they're cunning enough to figure it out", getDialogueHandler()));
 	}
 }
