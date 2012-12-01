d5a5c039fc04765d1511e7a56073c9b111ae23fd
yu.james1996@gmail.com <yu.james1996@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-01 17:37:15 +0000

    Made the first ogre room for you with proper game design in mind (progressive obstacles that let you see what's going on, teach you how to get past them, dialogue that actually adds to the story, and terrain that's both interesting, and teaches you how to double-jump).

--- trunk/src/dmcigd/levels/ogre/One.java
+++ trunk/src/dmcigd/levels/ogre/One.java
@@ -1,8 +1,10 @@
 package dmcigd.levels.ogre;
 
+import dmcigd.core.objects.blocks.CrumblingBlock;
 import dmcigd.core.objects.npc.Knight;
 import dmcigd.core.objects.npc.Sign;
 import dmcigd.core.room.Room;
+import dmcigd.levels.rabbit.Flag;
 
 import java.net.URL;
 
@@ -13,10 +15,19 @@ public class One extends Room implements Runnable{
 	}
 	
 	public void initializeRoom() {
-		addRegion(new Sign(73 * 32, 21 * 32, 6, "SIGN", "Entering Ogre Territory.\nWATCH YOUR STEP!", getDialogueHandler()));
+		addRegion(new Sign(26 * 32, 13 * 32, 9, "SIGN", "Entering Ogre Territory.\nWATCH YOUR STEP!", getDialogueHandler()));
 		
-		addRegion(new Knight(50 * 32 - 4, 15 * 32, false, "Congratulations on getting past the first part of your quest - unfortunately, I have to notify you that our forces are more scattered around these parts.", getDialogueHandler()));
-		addRegion(new Knight(142 * 32 - 4, 23 * 32, false, "Ogres lurk about - they may seem timid and frightened but they're really sly and cunning creatures. /nThe hole before you is an ogre trap - they set these up to catch their prey", getDialogueHandler()));
-		addRegion(new Knight(162 * 32 - 4, 24 * 32, false, "How do Ogres remove their prey from their traps? Well, um. That is a very good question. /nWell, I suppose they're cunning enough to figure it out", getDialogueHandler()));
+		addBackgroundObject(new Flag(53 * 32 + 16, 19 * 32 + 2 ,0));
+		addRegion(new Knight(53 * 32 + 4, 21 * 32, true, "Congratulations on getting past the first part of your quest! But we're still haven't reached the forest yet! We're going to have to make our way through the treacherous ogre camps to get there...", getDialogueHandler()));
+		
+		addBackgroundObject(new Flag(57 * 32 - 16, 19 * 32 + 2 ,1));
+		addRegion(new Knight(57 * 32 - 4, 21 * 32, true, "Don't be fooled by their timid appearance, the ogres are a brutish and savage bunch, and they're only out for your flesh.", getDialogueHandler()));
+		
+		addRegion(new Knight(81 * 32, 21 * 32, false, "Ogres lurk about - they may seem timid and frightened but they're really sly and cunning creatures. \nThe hole before you is an ogre trap - they set these up to catch their prey", getDialogueHandler()));
+		
+		addRegion(new Knight(109 * 32, 17 * 32, false, "You must be very careful around these parts, for their traps are fairly wide - they were made for prey even larger than us! They're often much longer than a single bound", getDialogueHandler()));
+		
+		addSolidObject(new CrumblingBlock(132 * 32, 25 * 32, 0.2f, 500));
+		addRegion(new Knight(126 * 32, 23 * 32, false, "If you're having trouble getting over these traps, it may help to get a running start.", getDialogueHandler()));
 	}
 }
