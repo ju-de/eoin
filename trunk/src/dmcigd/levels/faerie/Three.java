f5f500a3543a449a8cd648814d54d3baccc77dfc
HenryAuYeung.Ay@gmail.com <HenryAuYeung.Ay@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-01 05:35:57 +0000

    Level 7 (faerie) part of room 1

---
+++ trunk/src/dmcigd/levels/faerie/Three.java
@@ -0,0 +1,17 @@
+package dmcigd.levels.faerie;
+
+import java.net.URL;
+
+import dmcigd.core.room.Room;
+
+
+public class Three extends Room implements Runnable {
+	
+	public Three(URL codeBase) {
+		super(codeBase, "faerie", "Three", "grassy");
+	}
+
+	public void initializeRoom() {
+		
+	}
+}
