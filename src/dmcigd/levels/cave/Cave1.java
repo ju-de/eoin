89ccc7caa6b5040b352260c84436905d5c0735b2
James Yu <jj6yu@uwaterloo.ca>
2012-12-01 23:08:02 +0000

    Made the cave level a lot prettier. Also made it easier by having the silver key unlock the hidden area instead of being necessary to pass (again, as much as I love that puzzle, and as wonderfully as it's designed with the key being visible from that end, it's too early in the game)

--- src/dmcigd/levels/cave/Cave1.java
+++ src/dmcigd/levels/cave/Cave1.java
@@ -16,23 +16,22 @@ public class Cave1 extends Room {
 
 	public void initializeRoom(){
 		//locked doors
-		addSolidObject(new LockedDoor(45*32, 34*32, 1));
-		addSolidObject(new LockedDoor(75*32, 34*32, 1));
-		addSolidObject(new LockedDoor(112*32, 4*32, 2));
+		addSolidObject(new LockedDoor(44*32, 37*32, 1));
+		addSolidObject(new LockedDoor(82*32, 38*32, 1));
+		addSolidObject(new LockedDoor(57*32, 11*32, 2));
 		
 		//keys
-		addItem(new DoorKey(56*32, 35*32, 1, getPhysicsHandler()));
-		addItem(new DoorKey(62*32, 36*32, 2, getPhysicsHandler()));
-		addItem(new DoorKey(74*32, 34*32, 1, getPhysicsHandler()));
+		addItem(new DoorKey(45*32, 37*32, 1, getPhysicsHandler()));
+		addItem(new DoorKey(69*32, 40*32, 2, getPhysicsHandler()));
+		addItem(new DoorKey(81*32, 38*32, 1, getPhysicsHandler()));
 		
 		//signs
-		addRegion(new Sign(13* 32, 7* 32, 9 , "SIGN", "Bat's Cave", getDialogueHandler()));
-		addRegion(new Sign(110* 32, 4* 32, 6 , "SIGN", "Giant Vampire Bat", getDialogueHandler()));
+		addRegion(new Sign(20* 32, 11* 32, 9 , "SIGN", "Bat's Cave", getDialogueHandler()));
 		
 		//knockback bats
 		
 		//
 		
-		addRegion(new RoomWarp (114*32, 4*32, 2*32, 1*32, "game.MainMenu"));
+		addRegion(new RoomWarp (127*32 + 16, 8*32, 2*32, 1*32, "game.MainMenu"));
 	}
 }
