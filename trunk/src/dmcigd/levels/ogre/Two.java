60ee30e42198b65cf3b5a513a6daa8bf815e184c
necrophiliator@gmail.com <necrophiliator@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-01 22:39:46 +0000

    Woo - no errors! - Alan.

--- trunk/src/dmcigd/levels/ogre/Two.java
+++ trunk/src/dmcigd/levels/ogre/Two.java
@@ -1,11 +1,14 @@
 package dmcigd.levels.ogre;
 
+import dmcigd.core.objects.blocks.CrumblingBlock;
+import dmcigd.core.room.Room;
+
 import java.net.URL;
 
 public class Two extends Room implements Runnable {
 
 	public Two(URL codeBase) {
-		super(codeBase, "ogre", "Two", "grassy");
+		super(codeBase, "ogre", "One", "grassy");
 	}
 
 	public void initializeRoom() {
