07df8664e11efc3714753de98f4177d3bf3d104a
belieber10423 <belieber10423@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-02 20:50:03 +0000

    Changed deer sublevel 2

--- trunk/src/dmcigd/levels/deer/Two.java
+++ trunk/src/dmcigd/levels/deer/Two.java
@@ -4,7 +4,7 @@ import java.net.URL;
 
 import dmcigd.core.room.*;
 import dmcigd.core.objects.platforms.MovingPlatform;
-import dmcigd.core.objects.blocks.CrumblingBlock;
+import dmcigd.core.objects.blocks.TimedBlock;
 import dmcigd.core.objects.blocks.LockedDoor;
 import dmcigd.core.objects.blocks.PushableBlock;
 import dmcigd.core.objects.items.DoorKey;
@@ -20,33 +20,33 @@ public class Two extends Room implements Runnable{
 	}
 	
 	public void initializeRoom() {
-		addSolidObject(new MovingPlatform(41 * 32, 29 * 32, 1, 4, (float)0.6, 7));
-		addSolidObject(new MovingPlatform(81 * 32, 5 * 32, 0, 3, 2, 18));
+		addSolidObject(new MovingPlatform(56 * 32, 13 * 32, 1, 2, 1, 3));
+		addSolidObject(new MovingPlatform(59 * 32, 12 * 32, 1, 2, (float)1.5, 4));
+		addSolidObject(new MovingPlatform(62 * 32, 12 * 32, 1, 2, 2, 4));
+		addSolidObject(new MovingPlatform(65 * 32, 13 * 32, 1, 2, (float)0.5, 3));
 		
-		addSolidObject(new CrumblingBlock(78 * 32, 34 * 32, (float)0.1, 500));
-		addSolidObject(new CrumblingBlock(79 * 32, 34 * 32, (float)0.1, 500));
-		addSolidObject(new CrumblingBlock(80 * 32, 34 * 32, (float)0.1, 500));
-		addSolidObject(new CrumblingBlock(81 * 32, 34 * 32, (float)0.1, 500));
-		addSolidObject(new CrumblingBlock(82 * 32, 34 * 32, (float)0.1, 500));
-		addSolidObject(new CrumblingBlock(83 * 32, 34 * 32, (float)0.1, 500));
-		addSolidObject(new CrumblingBlock(84 * 32, 34 * 32, (float)0.1, 500));
+		addSolidObject(new TimedBlock(77 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(78 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(79 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(80 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(81 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(82 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(83 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(84 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(85 * 32, 16 * 32, 1, 0, 250, 250));
+		addSolidObject(new TimedBlock(86 * 32, 16 * 32, 1, 0, 250, 250));
 		
-		addSolidObject(new PushableBlock(109 * 32, 14 * 32, getPhysicsHandler()));
+		addSolidObject(new PushableBlock(113 * 32, 6 * 32, getPhysicsHandler()));
 		
-		addItem(new DoorKey(92 * 32, 44 * 32, 1, getPhysicsHandler()));
-		addSolidObject(new LockedDoor(126 * 32, 10 * 32, 1));
+		addItem(new DoorKey(125 * 32, 12 * 32, 1, getPhysicsHandler()));
+		addSolidObject(new LockedDoor(130 * 32, 16 * 32, 1));
 		
-		addItem(new DoorKey(128 * 32, 10 * 32, 2, getPhysicsHandler()));
-		addSolidObject(new LockedDoor(130 * 32, 36 * 32, 2));
+		addRegion(new Sign(147 * 32, 18 * 32, 5, "SIGN", "Deer Village", getDialogueHandler()));
 		
-		addRegion(new Sign(148 * 32, 38 * 32, 5, "SIGN", "Deer Village", getDialogueHandler()));
+		addRegion(new Knight(20 * 32, 15 * 32, false, "These wolves travel in such large packs, don't they?", getDialogueHandler()));
+		addRegion(new Knight(142 * 32, 18 * 32, true, "The deer village is just ahead. Remember your mission and try not to get too involved with their affairs... the deer people can be quite strange at times.", getDialogueHandler()));
 		
-		addRegion(new Knight(20 * 32, 35 * 32, false, "These wolves travel in such large packs, don't they?", getDialogueHandler()));
-		addRegion(new Knight(102 * 32, 36 * 32, true, "Strange... it felt like a rock just fell on my head...", getDialogueHandler()));
-		addRegion(new Knight(125 * 32, 36 * 32, false, "Hmm... I thought I might've seen a flash of metal on the side of this cliff. Could it have been the key?", getDialogueHandler()));
-		addRegion(new Knight(138 * 32, 37 * 32, true, "The deer village is just ahead. Remember your mission and try not to get too involved with their affairs... the deer people can be quite strange at times.", getDialogueHandler()));
-		
-		addRegion(new RoomWarp(150 * 32, 0, 32, 41 * 32, "deer.Three"));
+		addRegion(new RoomWarp(150 * 32, 13 * 32, 32, 8 * 32, "deer.Three"));
 	}
 	
 }
