627828d8de7a80b82959ce6266fa270995d75972
jrsylightning <jrsylightning@gmail.com>
2012-12-02 07:55:53 +0000

    IceRoom2 fix 1
    -patched up some missing corner blocks
    
    -added 2nd path to key (lethal monsters, bunny placeholders)
    
    -moved key upwards for visibility
    
    -added/fixed some more decor
    
    -removed unnecessary platforms
    
    **GENERALLY CLEANED UP MY SHIT (fix or tell me if something is still not right)

--- src/dmcigd/levels/icecave/IceRoom2.java
+++ src/dmcigd/levels/icecave/IceRoom2.java
@@ -7,6 +7,7 @@ import dmcigd.core.objects.items.DoorKey;
 import dmcigd.core.objects.npc.Sign;
 import dmcigd.core.objects.regions.*;
 import dmcigd.core.room.*;
+import dmcigd.levels.rabbit.mobs.SmallMob;
 
 public class IceRoom2 extends Room implements Runnable{
 	public IceRoom2(URL codeBase) {
@@ -14,9 +15,33 @@ public class IceRoom2 extends Room implements Runnable{
 	}
 	public void initializeRoom(){
 		addRegion(new Sign(30 * 32, 24 * 32, 5, "DANGER", "Beware of pitfalls and falling icicles!", getDialogueHandler()));
-		addRegion(new Sign(102 * 32, 19 * 32, 2, "placeholder", "meltable ice wall goes here to ceiling", getDialogueHandler()));
+		addRegion(new Sign(102 * 32, 17 * 32, 2, "placeholder", "meltable ice wall goes here to ceiling", getDialogueHandler()));
+		addRegion(new Sign(55 * 32, 25 * 32, 9, "BEWARE", "Wolves below!", getDialogueHandler()));
 		
-		addItem(new DoorKey(96 * 32, 19 * 32, 1, getPhysicsHandler()));
+		addItem(new DoorKey(96 * 32, 16 * 32, 1, getPhysicsHandler()));
 		addSolidObject(new LockedDoor(160 * 32, 12 * 32, 1));
+		
+		addSolidObject(new SmallMob(58 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(59 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(60 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(61 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(63 * 32, 30 * 32, getPhysicsHandler()));
+		
+		addSolidObject(new SmallMob(77 * 32, 29 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(79 * 32, 29 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(81 * 32, 29 * 32, getPhysicsHandler()));
+		
+		addSolidObject(new SmallMob(92 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(94 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(96 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(98 * 32, 30 * 32, getPhysicsHandler()));
+		
+		addSolidObject(new SmallMob(108 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(110 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(112 * 32, 30 * 32, getPhysicsHandler()));
+		
+		addSolidObject(new SmallMob(117 * 32, 31 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(119 * 32, 31 * 32, getPhysicsHandler()));
+		addSolidObject(new SmallMob(121 * 32, 31 * 32, getPhysicsHandler()));
 	}
 }
