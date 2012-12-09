c10f560c631624d6ee0d1812c14c7ba41e369eb3
jrsylightning@gmail.com <jrsylightning@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-09 08:39:00 +0000

    IceRoom 4 complete (no decor + switch block error not working, someone help me solve that :S) Exception in thread "Thread-10" java.lang.NullPointerException 	at dmcigd.core.room.GameObjectHandler.addSolidObject(GameObjectHandler.java:60) 	at dmcigd.levels.icecave.IceRoom4.initializeRoom(IceRoom4.java:25) <-- that line might be different now 	at dmcigd.core.room.Room.run(Room.java:192) 	at java.lang.Thread.run(Unknown Source)
    IceRoom 5 level structure complete (no decor, no ,.=- blocks, either me or someone else can button mash those in later)
    
    - okay so i added in a bunch of crap while trying to get the switches to work my bad
    	- there's a switch.gif in gfx/objects/icecave
    	- there's a Switch.java and SwitchBlock.java in src/dmcigd/levels/icecave
    	(remove these or w.e is necessary)
    
    - oh and i patched some texture bug in IceRoom3 (actually i forgot what i did but it says i updated it soooo yeah.)

---
+++ trunk/src/dmcigd/levels/icecave/IceRoom5.java
@@ -0,0 +1,36 @@
+package dmcigd.levels.icecave;
+
+import java.net.URL;
+
+import dmcigd.core.objects.blocks.LockedDoor;
+import dmcigd.core.objects.items.DoorKey;
+import dmcigd.core.objects.regions.*;
+import dmcigd.core.room.*;
+
+public class IceRoom5 extends Room implements Runnable{
+	public IceRoom5(URL codeBase) {
+		super(codeBase, "icecave", "IceRoom5", "icy");
+	}
+	public void initializeRoom(){
+			
+		addSolidObject(new LockedDoor(90*32, 28*32, 1));
+		addItem(new DoorKey(55*32, 39*32, 1, getPhysicsHandler()));
+		
+		addSolidObject(new BreakBlock(124 * 32, 23 * 32, 0.15f, 500));
+		addSolidObject(new BreakBlock(125 * 32, 23 * 32, 0.15f, 500));
+		
+		addSolidObject(new BreakBlock(133 * 32, 20 * 32, 0.15f, 500));
+		addSolidObject(new BreakBlock(134 * 32, 20 * 32, 0.15f, 500));
+		
+		addSolidObject(new LockedDoor(92*32, 10*32, 2));
+		addItem(new DoorKey(108*32, 13*32, 2, getPhysicsHandler()));
+		addSolidObject(new LockedDoor(93*32, 10*32, 3));
+		addItem(new DoorKey(111*32, 13*32, 3, getPhysicsHandler()));
+		
+		addSolidObject(new BreakBlock(54 * 32, 10 * 32, 0.15f, 500));
+		
+		addSolidObject(new LockedDoor(20*32, 15*32, 1));
+		addItem(new DoorKey(52*32, 10*32, 1, getPhysicsHandler()));
+		
+	}
+}
