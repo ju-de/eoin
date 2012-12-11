2d2d8810483d15756f5a5f95619380bc82d48991
m33kcow@gmail.com <m33kcow@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-11 04:12:59 +0000

    Objects

--- trunk/src/dmcigd/levels/lake/One.java
+++ trunk/src/dmcigd/levels/lake/One.java
@@ -22,6 +22,9 @@ public class One extends Room implements Runnable {
 		addSolidObject(new FishMob(18 * 32, 12 * 32, getPhysicsHandler()));
 		addSolidObject(new Turtle(18 * 32, 18 * 32, this));
 		addSolidObject(new Turtle(16 * 32, 18 * 32, this)); */
+		addRegion(new Sign(29 * 32, 9 * 32, 5, "Welcome", "So now you're underwater... So have fun, explore and enjoy swimming around. Also, who needs air?", getDialogueHandler()));
+		addRegion(new Sign(55 * 32, 11 * 32, 3, "Warning", "Lake inhabitants include: Spikes... lots of them.", getDialogueHandler()));
+		
 		
 		addRegion(new RoomWarp(0, 49 * 32, 216 * 32, 2 * 32, "lake.Two"));
 	}
