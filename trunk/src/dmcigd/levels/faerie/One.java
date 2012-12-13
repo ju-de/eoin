0e6a76bf0c7769e618fc4b3b8c9949e6e0d7602a
henryauyeung.ay@gmail.com <henryauyeung.ay@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-13 22:03:25 +0000

    faerie level stuff

--- trunk/src/dmcigd/levels/faerie/One.java
+++ trunk/src/dmcigd/levels/faerie/One.java
@@ -2,7 +2,8 @@ package dmcigd.levels.faerie;
 
 import java.net.URL;
 
-import dmcigd.core.room.Room;
+import dmcigd.core.objects.blocks.*;
+import dmcigd.core.room.*;
 
 
 public class One extends Room implements Runnable {
@@ -12,6 +13,35 @@ public class One extends Room implements Runnable {
 	}
         
 	public void initializeRoom() {
-
+		
+		
+		addSolidObject(new CrumblingBlock(63 * 32, 49 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(66 * 32, 48 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(70 * 32, 47 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(73 * 32, 47 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(79 * 32, 49 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(82 * 32, 48 * 32, 0.2f, 500));
+		
+		addSolidObject(new CrumblingBlock(98 * 32, 49 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(99 * 32, 49 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(100 * 32, 49 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(101 * 32, 49 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(102 * 32, 49 * 32, 0.2f, 500));
+		
+		addSolidObject(new CrumblingBlock(117 * 32, 50 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(117 * 32, 51 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(117 * 32, 52 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(122 * 32, 52 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(122 * 32, 53 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(5127 * 32, 52 * 32, 0.2f, 500));
+		
+		addSolidObject(new CrumblingBlock(50 * 32, 146 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(41 * 32, 148 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(53 * 32, 151 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(55 * 32, 154 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(56 * 32, 161 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(56 * 32, 162 * 32, 0.2f, 500));
+		addSolidObject(new CrumblingBlock(57 * 32, 163 * 32, 0.2f, 500));
+		
 	}
 }
