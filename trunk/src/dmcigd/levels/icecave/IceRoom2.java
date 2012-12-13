4f2fb10b51f766cb6e287e7f2e6996834edb054d
yu.james1996@gmail.com <yu.james1996@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-13 18:53:31 +0000

    Added in "Ice Wolf" mob.

--- trunk/src/dmcigd/levels/icecave/IceRoom2.java
+++ trunk/src/dmcigd/levels/icecave/IceRoom2.java
@@ -7,7 +7,6 @@ import dmcigd.core.objects.items.DoorKey;
 import dmcigd.core.objects.npc.Sign;
 import dmcigd.core.objects.regions.*;
 import dmcigd.core.room.*;
-import dmcigd.levels.rabbit.mobs.SmallMob;
 
 public class IceRoom2 extends Room implements Runnable{
 	public IceRoom2(URL codeBase) {
@@ -20,27 +19,21 @@ public class IceRoom2 extends Room implements Runnable{
 		addItem(new DoorKey(96 * 32, 16 * 32, 1, getPhysicsHandler()));
 		addSolidObject(new LockedDoor(160 * 32, 12 * 32, 1));
 		
-		addSolidObject(new SmallMob(58 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(59 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(60 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(61 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(63 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new IceWolf(58 * 32, 30 * 32, this));
+		addSolidObject(new IceWolf(60 * 32, 30 * 32, this));
+		addSolidObject(new IceWolf(63 * 32, 30 * 32, this));
 		
-		addSolidObject(new SmallMob(77 * 32, 29 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(79 * 32, 29 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(81 * 32, 29 * 32, getPhysicsHandler()));
+		addSolidObject(new IceWolf(77 * 32, 29 * 32, this));
+		addSolidObject(new IceWolf(81 * 32, 29 * 32, this));
 		
-		addSolidObject(new SmallMob(92 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(94 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(96 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(98 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new IceWolf(92 * 32, 30 * 32, this));
+		addSolidObject(new IceWolf(94 * 32, 30 * 32, this));
+		addSolidObject(new IceWolf(98 * 32, 30 * 32, this));
 		
-		addSolidObject(new SmallMob(108 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(110 * 32, 30 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(112 * 32, 30 * 32, getPhysicsHandler()));
+		addSolidObject(new IceWolf(108 * 32, 30 * 32, this));
+		addSolidObject(new IceWolf(112 * 32, 30 * 32, this));
 		
-		addSolidObject(new SmallMob(117 * 32, 31 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(119 * 32, 31 * 32, getPhysicsHandler()));
-		addSolidObject(new SmallMob(121 * 32, 31 * 32, getPhysicsHandler()));
+		addSolidObject(new IceWolf(117 * 32, 31 * 32, this));
+		addSolidObject(new IceWolf(121 * 32, 31 * 32, this));
 	}
 }
---
+++ trunk/src/dmcigd/levels/icecave/IceWolf.java
@@ -0,0 +1,196 @@
+package dmcigd.levels.icecave;
+
+import dmcigd.core.enums.CollisionType;
+import dmcigd.core.enums.Direction;
+import dmcigd.core.enums.EntityType;
+import dmcigd.core.objects.interfaces.*;
+import dmcigd.core.room.Room;
+import dmcigd.core.objects.monsters.*;
+import dmcigd.levels.deer.mobs.AttackProjectile;
+
+public class IceWolf extends HitpointHandler implements RestableObject {
+	
+	private boolean isAttacking,isJumping;
+	private Room room;
+	private int deathClock = 0;
+	private int aiClock = 0;
+	private int deathClockLimit = 70;
+	private int aiClockReset = 40;
+
+	public void attack() {
+		setSequence(2);
+		isAttacking = true;
+		accelerate(32, 0, Direction.RIGHT);
+		setFrameSpeed(0.12f);
+		setVX(0);
+	}
+	
+	public void flicker(boolean flicker) {
+		setSequence(0);
+		if(flicker) {
+			setFrame(0);
+		}else{
+			setFrame(2);
+		}
+	}
+	
+	public void idle() {
+		setSequence(0);
+		setFrameSpeed(0.02f);
+		accelerate(32, 0, Direction.RIGHT);
+		setVX(0);
+	}
+	
+	public void moveLeft() {
+		flipped = true;
+		accelerate(0.3f, 2.4f, Direction.LEFT);
+		setSequence(1);
+		setFrameSpeed(0.12f);
+		if(Math.random() < 0.3f) {
+			setVY(-5.6f);
+			isJumping = true;
+		}
+	}
+	
+	public void moveRight() {
+		flipped = false;
+		accelerate(0.3f, 2.4f, Direction.RIGHT);
+		setSequence(1);
+		setFrameSpeed(0.12f);
+		if(Math.random() < 0.3f) {
+			setVY(-5.6f);
+			isJumping = true;
+		}
+	}
+
+	public void die() {
+		setSequence(3);
+		setFrameSpeed(0.2f);
+		setCollisionType(CollisionType.NONSOLID);
+	}
+	
+	public boolean isDestroyed() {
+		if(isDestroyed && deathClock >= deathClockLimit) {
+			return true;
+		}
+		return false;
+	}
+
+	public IceWolf(int x, int y, Room room) {
+	
+		setX(x);
+		setY(y);
+		setWidth(36);
+		setHeight(26);
+		setImageWidth(48);
+		setImageHeight(36);
+		
+		setSequence(0);
+		setFrame(0);
+	
+		setFrameLimits(new int[] {2, 4, 5, 4});
+		setAnimationLoops(new boolean [] {true, true, false, false});
+		setFrameSpeed(0.02f);
+		
+		setMaxHitpoints(70);
+		setKnockback(true);
+		
+		this.room = room;
+		
+		setGravity();
+		
+		setPhysicsHandler(room.getPhysicsHandler());
+		setCollisionType(CollisionType.SOLID);
+		setEntityType(EntityType.NONLETHALMONSTER);
+		
+		setImagePath("objects/icecave/icewolf.gif");
+	}
+	
+	public void think() {
+		
+		if(isDestroyed) {
+			//Increment death animation timer
+			if(deathClock == 0) {
+				die();
+			}
+			deathClock++;
+		} else if(isInvincible()) {
+			//Flinch (overrides usual timer)
+			if(isKnockedBack()) {
+				flicker(isFlickering());
+			} else {
+				idle();
+			}
+			aiClock = 0;
+			isAttacking = false;
+		} else if(isAttacking) {
+			aiClock++;
+			if(aiClock == 16) {
+				//Create attack projectile
+				int x = (int) getX();
+				if(!flipped) {
+					x += getWidth();
+				} else {
+					x -= 4;
+				}
+				room.addProjectile(new AttackProjectile(x, (int) getY(), getHeight(), room, 10));
+			}
+			if(getFrame() == 4) {
+				isAttacking = false;
+			}
+		} else if(isJumping) {
+			if(hitGround) {
+				isJumping = false;
+			}
+		} else {
+			//Proceeds with typical AI
+			aiClock++;
+			if(aiClock >= aiClockReset) {
+				//Makes a random decision
+				int decision = (int) (Math.random() * 10);
+				//Player Distance
+				float playerDistance = room.getPlayer().getX() - getX();
+				if(playerDistance < 400 && playerDistance > - 400) {
+					//Pursue player
+					if(decision < 3) {
+						attack();
+					} else {
+						if(playerDistance >= 0) {
+							moveRight();
+						} else {
+							moveLeft();
+						}
+					}
+				} else {
+					//Wander
+					if(decision < 2) {
+						moveRight();
+					}else if(decision < 4) {
+						moveLeft();
+					}else if(decision < 8) {
+						attack();
+					}else {
+						idle();
+					}
+				}
+				
+				aiClock = 0;
+			}
+		}
+		
+	}
+	
+	public void step() {
+		
+		super.step();
+		
+		think();
+		
+		move();
+		
+		if(!isKnockedBack()) {
+			animate();
+		}
+	}
+
+}
