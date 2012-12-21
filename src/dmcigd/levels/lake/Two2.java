e96eafefbe639e0fac2eee0223e078de6304b954
m33kcow <m33kcow@gmail.com>
2012-12-21 03:59:57 +0000

    Fixed Turtle challenge

--- src/dmcigd/levels/lake/Two2.java
+++ src/dmcigd/levels/lake/Two2.java
@@ -16,7 +16,7 @@ public class Two2 extends Room implements Runnable {
 	
 	public void initializeRoom() {
 		
-		addRegion(new Sign(30 * 32, 29 * 32 ,9 , "Tip", "It's all about timing.", getDialogueHandler()));
+		addRegion(new Sign(37 * 32, 20 * 32 , 5, "Tip", "It's all about the timing", getDialogueHandler()));
 		
 		//first hallway
 		addSolidObject(new FishSchool(38 * 32, 19 * 32, this));
@@ -39,7 +39,7 @@ public class Two2 extends Room implements Runnable {
 		addSolidObject(new LockedDoor(57 * 32, 28 * 32, 1));
 		
 		//second door
-		addItem(new DoorKey(34 * 32, 20 * 32, 2, getPhysicsHandler()));
+		addItem(new DoorKey(41 * 32, 19 * 32, 2, getPhysicsHandler()));
 		addSolidObject(new LockedDoor(59 * 32, 30 * 32, 2));
 		
 		//third bit
@@ -63,25 +63,25 @@ public class Two2 extends Room implements Runnable {
 		addItem(new DoorKey(58 * 32, 58 * 32, 2, getPhysicsHandler()));
 		addSolidObject(new LockedDoor(77 * 32, 50 * 32, 2));
 		
-		addSolidObject(new FishSchool(58 * 32, 44* 32, this));
-		addSolidObject(new FishSchool(61 * 32, 43 * 32, this));
-		addSolidObject(new FishSchool(46 * 32, 45 * 32, this));
+		addSolidObject(new FishSchool(53 * 32, 44 * 32, this));
+		addSolidObject(new FishSchool(55 * 32, 45 * 32, this));
 		
+		addSolidObject(new FishMob(58 * 32, 45 * 32, getPhysicsHandler(), getPlayer()));
+		addSolidObject(new FishMob(43 * 32, 46 * 32, getPhysicsHandler(), getPlayer()));
 		addSolidObject(new FishMob(41 * 32, 47 * 32, getPhysicsHandler(), getPlayer()));
 		
-		addRegion(new Sign(47 * 32, 51 * 32 , 7, "Carful.", "Use the turtles to your advantage.", getDialogueHandler()));
+		addRegion(new Sign(47 * 32, 51 * 32 , 7, "Caution", "Use the turtles to your advantage", getDialogueHandler()));
 		addSolidObject(new FishSchool(48 * 32, 50* 32, this));
 		
-		addSolidObject(new Turtle(32 * 32, 53 * 32,  this));
-		addSolidObject(new Turtle(34 * 32, 53 * 32,  this));
-		addSolidObject(new Turtle(36 * 32, 53 * 32,  this));
-		addSolidObject(new Turtle(39 * 32, 53 * 32,  this));
-		addSolidObject(new Turtle(41 * 32, 53 * 32,  this));
+		addSolidObject(new Turtle(44 * 32, 53 * 32,  this));
 		addSolidObject(new Turtle(43 * 32, 53 * 32,  this));
-		addSolidObject(new Turtle(45 * 32, 53 * 32,  this));
-		addSolidObject(new Turtle(46 * 32, 70 * 32,  this));
-		addSolidObject(new Turtle(47 * 32, 53 * 32,  this));
-		addSolidObject(new Turtle(49 * 32, 70 * 32,  this));
+		addSolidObject(new Turtle(41 * 32, 53 * 32,  this));
+		addSolidObject(new Turtle(40 * 32, 53 * 32,  this));
+		addSolidObject(new Turtle(38 * 32, 53 * 32,  this));
+		addSolidObject(new Turtle(37 * 32, 53 * 32,  this));
+		addSolidObject(new Turtle(35 * 32, 53 * 32,  this));
+		addSolidObject(new Turtle(33 * 32, 53 * 32,  this));
+		addSolidObject(new Turtle(32 * 32, 53 * 32,  this));
 		
 		
 		//second key room
@@ -90,22 +90,18 @@ public class Two2 extends Room implements Runnable {
 		
 		//turtlemass
 		addRegion(new Sign(48 * 32, 62 * 32 , 7, "Carful.", "You arn't strong enough to push more than one turtle shell at a time.", getDialogueHandler()));
-		addSolidObject(new Turtle(50 * 32, 70 * 32,  this));
+		addSolidObject(new Turtle(48 * 32, 70 * 32,  this));
 		addSolidObject(new Turtle(51 * 32, 70 * 32,  this));
-		addSolidObject(new Turtle(50 * 32, 70 * 32,  this));
-		addSolidObject(new Turtle(52 * 32, 70 * 32,  this));
-		
-		addSolidObject(new Turtle(53 * 32, 70 * 32,  this));
 		addSolidObject(new Turtle(54 * 32, 70 * 32,  this));
 		
-		addSolidObject(new Turtle(59 * 32, 70 * 32,  this));
-		addSolidObject(new Turtle(60 * 32, 70 * 32,  this));
-		addSolidObject(new Turtle(61 * 32, 70 * 32,  this));
 		addSolidObject(new Turtle(62 * 32, 70 * 32,  this));
-		addSolidObject(new Turtle(63 * 32, 70 * 32,  this));
-		addSolidObject(new Turtle(87 * 32, 70 * 32,  this));
+		addSolidObject(new Turtle(64 * 32, 70 * 32,  this));
+		addSolidObject(new Turtle(66 * 32, 70 * 32,  this));
+		addSolidObject(new Turtle(68 * 32, 70 * 32,  this));
+		addSolidObject(new Turtle(73 * 32, 70 * 32,  this));
+		addSolidObject(new Turtle(75 * 32, 70 * 32,  this));
+		addSolidObject(new Turtle(86 * 32, 70 * 32,  this));
 		addSolidObject(new Turtle(88 * 32, 70 * 32,  this));
-		addSolidObject(new Turtle(89 * 32, 70 * 32,  this));
 		addSolidObject(new Turtle(90 * 32, 70 * 32,  this));
 		
 		// third key
