0c0988c2d37039e65846beb3e591f494971eaf01
jrsylightning <jrsylightning@gmail.com>
2012-12-22 07:39:23 +0000

    - ICE CAVE LEVEL UPDATE 	- added falling icicles to rooms 2,3,4,5 	- added BOSS ROOM w/o boss, w/o ,.-=blocks (sprinting scene) 		- assume you have to sprint continuously cuz you're being chased by a wendigo (which isn't there yet).
    OKAY I AIN'T ADDING FALLING ICICLES TO BOSS ROOM, IT'S HARD ENOUGH AS IS{
    	
    	let me just explain how the game mechanic works, because while I was figuring it out, it was frustrating as fuck.
    	
    	SO, LADDER PARKOUR (ability to sprint off a ladder and continue jumping to do platforming):
    	
    	-how this essentially works is that, the moment you hit a ladder (while holding sprint and jumping at it), if you press jump again the MOMENT 
    	your character gets off of the ladder, you will perform what I like to call ladder parkour. Now, the timing on this is extremely tricky, 
    	so you'll probably die a fuckton while trying it out (as I did.) The main concern about this is, after getting off the ladder, you may only be able
    	to jump ONCE, not DOUBLE JUMP (although doublejump works in most cases, I have times where it only allows 1 jump after getting off the ladder, 
    	therefore, assume you only get 1 jump -- the level is still do-able), 
    	so make sure to time your jumps properly, or risk falling into the pits of death. 
    	
    	Oh and, the level IS BEATABLE; you'll need a fairly quick reaction time though.
    
    }

--- src/dmcigd/levels/icecave/IceRoom2.java
+++ src/dmcigd/levels/icecave/IceRoom2.java
@@ -13,6 +13,19 @@ public class IceRoom2 extends Room implements Runnable{
 		super(codeBase, "icecave", "IceRoom2", "icy");
 	}
 	public void initializeRoom(){
+		//INI game objects
+		IcicleProjectile icicle1 = new IcicleProjectile(77 * 32, 9 * 32, getBlockMap(), getSolidObjects());
+		addSolidObject(icicle1);
+		addRegion(new IcicleTrigger(74 * 32, 9 * 32, 1, 3, icicle1));
+		
+		IcicleProjectile icicle2 = new IcicleProjectile(95 * 32, 9 * 32, getBlockMap(), getSolidObjects());
+		addSolidObject(icicle2);
+		addRegion(new IcicleTrigger(93 * 32, 8 * 32, 1, 5, icicle2));
+		
+		IcicleProjectile icicle3 = new IcicleProjectile(180 * 32, 10 * 32, getBlockMap(), getSolidObjects());
+		addSolidObject(icicle3);
+		addRegion(new IcicleTrigger(176 * 32, 11 * 32, 1, 2, icicle3));
+		
 		addRegion(new Sign(30 * 32, 24 * 32, 5, "DANGER", "Beware of pitfalls and falling icicles!", getDialogueHandler()));
 		addRegion(new Sign(55 * 32, 25 * 32, 9, "BEWARE", "Wolves below!", getDialogueHandler()));
 		
@@ -35,5 +48,7 @@ public class IceRoom2 extends Room implements Runnable{
 		
 		addSolidObject(new IceWolf(117 * 32, 31 * 32, this));
 		addSolidObject(new IceWolf(121 * 32, 31 * 32, this));
+		
+		addRegion(new RoomWarp(193 * 32, 13 * 32, 1 * 32, 2 * 32, "icecave.IceRoom3"));
 	}
 }
