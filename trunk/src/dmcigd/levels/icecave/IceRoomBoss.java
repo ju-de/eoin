fa4f2b03748766257c33395cbc29c864de3e77ae
jrsylightning@gmail.com <jrsylightning@gmail.com@a3e1f297-2536-abf2-aca5-e57074268ef5>
2012-12-22 22:56:25 +0000

    Swapping computers, need to update (ignore this change for now)

--- trunk/src/dmcigd/levels/icecave/IceRoomBoss.java
+++ trunk/src/dmcigd/levels/icecave/IceRoomBoss.java
@@ -12,8 +12,19 @@ public class IceRoomBoss extends Room implements Runnable{
 		super(codeBase, "icecave", "IceRoomBoss", "icy");
 	}
 	public void initializeRoom(){
+		//INI game objects
+		IcicleProjectile icicle1 = new IcicleProjectile(76 * 32, 16 * 32, getBlockMap(), getSolidObjects());
+		addSolidObject(icicle1);
+		addRegion(new IcicleTrigger(73 * 32, 22 * 32, 1, 1, icicle1));
 		
+		IcicleProjectile icicle2 = new IcicleProjectile(82 * 32, 15 * 32, getBlockMap(), getSolidObjects());
+		addSolidObject(icicle2);
+		addRegion(new IcicleTrigger(79 * 32, 22 * 32, 1, 1, icicle2));
 		
-		//addRegion(new RoomWarp(388 * 32, 11 * 32, 1 * 32, 1 * 32, "nextlevel.NextLevel"));
+		IcicleProjectile icicle3 = new IcicleProjectile(90 * 32, 15 * 32, getBlockMap(), getSolidObjects());
+		addSolidObject(icicle3);
+		addRegion(new IcicleTrigger(87 * 32, 22 * 32, 1, 1, icicle3));
+		
+		addRegion(new RoomWarp(388 * 32, 11 * 32, 1 * 32, 1 * 32, "game.MainMenu"));
 	}
 }
